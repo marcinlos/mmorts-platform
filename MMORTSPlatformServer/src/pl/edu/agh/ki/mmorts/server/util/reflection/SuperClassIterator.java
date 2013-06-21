f22732b76282c642c4c1ba30d5ba146f4da7528d
los <losiu99@gazeta.pl>
2013-06-21 20:52:54 +0200

    Restored reflection files (accidently, I suppose) deleted by drew

---
+++ MMORTSPlatformServer/src/pl/edu/agh/ki/mmorts/server/util/reflection/SuperClassIterator.java
@@ -0,0 +1,38 @@
+package pl.edu.agh.ki.mmorts.server.util.reflection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Iterator yielding all the superclasses of a given class (including this 
+ * class itself). 
+ * 
+ * @author los
+ */
+public class SuperClassIterator implements Iterator<Class<?>> {
+    
+    private Class<?> clazz;
+
+    public SuperClassIterator(Class<?> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return clazz.getSuperclass() != null;
+    }
+
+    @Override
+    public Class<?> next() {
+        if (! hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return clazz = clazz.getSuperclass();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Cannot modify class hierarchy");
+    }
+
+}
