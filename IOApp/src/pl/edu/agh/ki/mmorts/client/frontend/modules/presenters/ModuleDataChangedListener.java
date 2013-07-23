a72957541b6006670e9b51cafe5f69657c817f27
kasia <klasak5@gmail.com>
2013-07-23 18:44:03 +0200

    Interfaces in frontend changed

---
+++ IOApp/src/pl/edu/agh/ki/mmorts/client/frontend/modules/presenters/ModuleDataChangedListener.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.ki.mmorts.client.frontend.modules.presenters;
+
+public interface ModuleDataChangedListener {
+	/**
+	 * method used as a listener in module - receives concrete object with new data.
+	 */
+	public void dataChanged(ModuleDataMessage data); //TODO sialalala
+
+}
