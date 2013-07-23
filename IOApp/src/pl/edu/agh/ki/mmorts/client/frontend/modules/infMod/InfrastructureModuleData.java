ca0c79840c5d07fce778fc52e1373645a411e70e
drew <re7roniq@gmail.com>
2013-07-23 21:11:58 +0200

    Windows non-case-sensitiveness rapaired. InfMod changed to infMod

---
+++ IOApp/src/pl/edu/agh/ki/mmorts/client/frontend/modules/infMod/InfrastructureModuleData.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.ki.mmorts.client.frontend.modules.infMod;
+
+public class InfrastructureModuleData {
+
+	public boolean isSpaceAvailable;
+	public int mapHeight;
+	public int mapWidth;
+	public ITile[][] map;
+	
+	
+}
