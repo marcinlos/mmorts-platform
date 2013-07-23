a455af13b3cc79e30fc638a07420ca502b295f83
kasia <klasak5@gmail.com>
2013-07-23 19:12:29 +0200

    AbstractModulePresenter ready (I think)

--- IOApp/src/pl/edu/agh/ki/mmorts/client/frontend/modules/presenters/AbstractModulePresenter.java
+++ IOApp/src/pl/edu/agh/ki/mmorts/client/frontend/modules/presenters/AbstractModulePresenter.java
@@ -3,11 +3,17 @@ package pl.edu.agh.ki.mmorts.client.frontend.modules.presenters;
 import com.google.inject.Inject;
 
 import pl.edu.agh.ki.mmorts.client.frontend.modules.ModulesBroker;
+import pl.edu.agh.ki.mmorts.client.frontend.spaceManaging.MainSpaceManager;
+import pl.edu.agh.ki.mmorts.client.frontend.spaceManaging.TopSpaceManager;
 
-public abstract class AbstractModulePresenter implements ModulePresenter{
+public abstract class AbstractModulePresenter implements ModulePresenter, ModuleDataChangedListener, BusListener{
 	
 	@Inject
 	protected ModulesBroker modulesBroker;
+	@Inject
+	protected MainSpaceManager mainSpaceManager;
+	@Inject
+	protected TopSpaceManager topSpaceManager;
 	protected String presenterId; 
 
 }
