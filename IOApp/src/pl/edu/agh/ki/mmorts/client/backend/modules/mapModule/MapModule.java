7117ed0cd115b205698e8ac1cdb03ee0758c3906
kasia <klasak5@gmail.com>
2013-07-30 21:37:48 +0200

    Usuniete infrastructure

--- IOApp/src/pl/edu/agh/ki/mmorts/client/backend/modules/mapModule/MapModule.java
+++ IOApp/src/pl/edu/agh/ki/mmorts/client/backend/modules/mapModule/MapModule.java
@@ -1,14 +1,22 @@
 package pl.edu.agh.ki.mmorts.client.backend.modules.mapModule;
 
+import com.google.inject.Inject;
+
 import pl.edu.agh.ki.mmorts.client.backend.modules.ModuleBase;
 import pl.edu.agh.ki.mmorts.client.frontend.modules.GUICommModule;
+import pl.edu.agh.ki.mmorts.client.frontend.modules.ModulesBroker;
 import pl.edu.agh.ki.mmorts.client.messages.ModuleDataMessage;
 
 public class MapModule extends ModuleBase implements GUICommModule{
+	
+	private static final String ID = "MapModule";
+	
+	@Inject(optional= true)
+	ModulesBroker modulesBroker;
 
 	@Override
 	public void dataChanged(ModuleDataMessage data) {
-		// TODO Auto-generated method stub
+		// jak w BuildingModule
 		
 	}
 	
