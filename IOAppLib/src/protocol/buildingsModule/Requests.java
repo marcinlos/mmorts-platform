57feafb85956827309026191ee944032e45eca1e
kasia <klasak5@gmail.com>
2013-08-02 20:04:00 +0200

    BuildingModule communication protocol added. Get buildings added. Hope it works.

---
+++ IOAppLib/src/protocol/buildingsModule/Requests.java
@@ -0,0 +1,17 @@
+package protocol.buildingsModule;
+
+public class Requests {
+	
+	public static final String CAN_BUILD = "can-build";
+	public static final String FULL_INTERNAL = "full";
+	public static final String YES_CAN_BUILD = "yes-can-build";
+	public static final String NO_CAN_BUILD = "no-can-build";
+	public static final String BUILD = "build";
+	public static final String BUILD_SUCCESS = "build-success";
+	public static final String PUT_ON = "putOn";
+	public static final String GET_BUILDINGS = "get-buildings";
+	public static final String DEMOLISH = "demolish";
+	public static final String DEMOLISH_SUCCESS = "demolish-success";
+	public static final String REL_AT = "releaseAt";
+	
+}
